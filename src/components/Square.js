b0799ac88f1ab317cb5f95c42679a03d0b822d6b
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-05-10 21:41:35 -0300

    feat: use dispatch in Square Component

--- src/components/Square.js
+++ src/components/Square.js
@@ -3,10 +3,25 @@ import PropTypes from "prop-types";
 import { GameContext } from "../contexts/GameContext";
 
 export default function Square({ value, index }) {
-  const { handlePlayerAction } = useContext(GameContext);
+  const {
+    state: { squares, whoIsWinner, isXNext },
+    dispatch,
+  } = useContext(GameContext);
+
+  function handleClick() {
+    if (squares[index]) return;
+    if (whoIsWinner) return;
+
+    const newSquares = [...squares];
+    newSquares[index] = isXNext ? "X" : "O";
+    dispatch({
+      type: "UPDATE_SQUARES",
+      payload: newSquares,
+    });
+  }
 
   return (
-    <button type="button" onClick={() => handlePlayerAction(index)}>
+    <button type="button" onClick={handleClick}>
       {value}
     </button>
   );
