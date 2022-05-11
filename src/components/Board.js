b382ac3a847a1a55faa8b951076747bd89055168
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-05-10 22:06:13 -0300

    refactor: Board component

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { v4 as uuid } from "uuid";
 import { GameContext } from "../contexts/GameContext";
 import History from "./History";
@@ -6,9 +6,23 @@ import Player from "./Player";
 import Reset from "./Reset";
 import Square from "./Square";
 import Winner from "./Winner";
+import calculateWinner from "../utils/calculateWinner";
 
 export default function Board() {
-  const { squares } = useContext(GameContext);
+  const {
+    state: { squares },
+    dispatch,
+  } = useContext(GameContext);
+
+  useEffect(() => {
+    const winner = calculateWinner(squares);
+
+    if (winner)
+      dispatch({
+        type: "UPDATE_WINNER",
+        payload: winner,
+      });
+  }, [squares, dispatch]);
 
   return (
     <div className="board-container">
